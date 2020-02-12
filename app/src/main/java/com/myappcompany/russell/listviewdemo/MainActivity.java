1dd71e536a6187a14c70abdd9eaa80719129411b
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-12 07:59:34 -1000

    final commit

--- app/src/main/java/com/myappcompany/russell/listviewdemo/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/listviewdemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,23 +18,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Set array
         ListView myListView = findViewById(R.id.myListView);
 
-        final ArrayList<String> myFamily = new ArrayList<String>();
+        //Making array of friends
+        final ArrayList<String> myFriends = new ArrayList<String>();
 
-        myFamily.add("Mom");
-        myFamily.add("Dad");
-        myFamily.add("Brother");
-        myFamily.add("Dog");
+        myFriends.add("john");
+        myFriends.add("sara");
+        myFriends.add("megan");
+        myFriends.add("josh");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
+        //Converts array to be shown in ListView
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFriends);
 
+        //Shows the ListView
         myListView.setAdapter(arrayAdapter);
 
+        //Allows you to click on a friend and display Toast
         myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.i("Person selected: ", myFamily.get(i));
+                Toast.makeText(getApplicationContext(),"Friend selected: " + myFriends.get(i), Toast.LENGTH_LONG).show();
             }
         });
     }
